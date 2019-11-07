1c70866b7fa9aed5145838a50bdf24aa8bb951af
wictor <wisv1500@student.miun.se>
2019-11-07 14:43:07 +0100

    created JavaHTTPServer file

---
+++ src/main/java/JavaHTTPServer.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class JavaHTTPServer {
+
+    static final int PORT = 8080;
+
+    public static void main(String[] args){
+        final ServerSocket server = new ServerSocket(PORT);
+        system.out.println("listening");
+        while(true){
+            //hej
+        }
+    }
+
+}
